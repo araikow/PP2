eb9240ffa5dd4ad0831887c2af24633271ed4a7d
araikow <askarbekovnav@gmail.com>
2019-01-31 08:44:40 +0600

    Task3 true version

---
+++ Week1/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            string elements = Console.ReadLine();
+            string[] array = elements.Split();
+
+            int n = int.Parse(number);
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+                Console.Write(array[i]);
+                Console.Write(" ");
+            }
+            Console.ReadKey();
+         }   
+    }
+}
