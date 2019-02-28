7d4a02bbf695be60bb7adb65bc384335d28241ed
araikow <askarbekovnav@gmail.com>
2019-02-28 23:52:20 +0600

    Task1

---
+++ TSIS4/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+    public class Complex
+    {
+        public int real;
+        public int imaginary;
+
+        public Complex(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+        public Complex() { }
+        public override string ToString()
+        {
+            return this.real + " " + "+" + " " + this.imaginary+"i";
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c = new Complex(4,5);
+            FileStream fs = new FileStream("complex.txt", FileMode.Create, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            xs.Serialize(fs, c);
+            fs.Close();
+            
+
+            FileStream fs1 = new FileStream("complex.txt", FileMode.Open, FileAccess.Read);
+            XmlSerializer xs1 = new XmlSerializer(typeof(Complex));
+            Complex c1 = xs.Deserialize(fs1) as Complex;
+            Console.WriteLine(c1.ToString()); 
+            fs1.Close();
+            Console.ReadKey();
+        }
+    }
+}
